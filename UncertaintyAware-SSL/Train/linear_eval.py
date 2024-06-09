feat: mixed precision and std fix

UncertaintyAware-SSL/Train/linear_eval.py
@@ -6,6 +6,7 @@ from models.ugraft import UGraft, LinearClassifier, model_dict
 import torch.backends.cudnn as cudnn
 from torch import nn
 from sklearn.metrics import classification_report
+from torch.cuda.amp import GradScaler, autocast
 
 
 
@@ -61,6 +62,7 @@ def set_model_linear(number_cls, path, nh=5, opt=None, image_size=None):
 
     return model, classifier, criterion
 
+scaler = GradScaler()
 
 def train(train_loader, model, classifier, criterion, optimizer, epoch, opt):
     """one epoch training"""
@@ -86,13 +88,14 @@ def train(train_loader, model, classifier, criterion, optimizer, epoch, opt):
         warmup_learning_rate(opt, epoch, idx, len(train_loader), optimizer)
 
         # compute loss
-        with torch.no_grad():
-            if opt.ugraft_probing:
-                features, _ = model(images)
-            else:
-                features = model.encoder(images)
-        output = classifier(features.detach())
-        loss = criterion(output, labels)
+        with autocast():
+            with torch.no_grad():
+                if opt.ugraft_probing:
+                    features, _ = model(images)
+                else:
+                    features = model.encoder(images)
+            output = classifier(features.detach())
+            loss = criterion(output, labels)
 
         # update metric
         losses.update(loss.item(), bsz)
@@ -102,8 +105,9 @@ def train(train_loader, model, classifier, criterion, optimizer, epoch, opt):
 
         # SGD
         optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
 
         # measure elapsed time
         batch_time.update(time.time() - end)

UncertaintyAware-SSL/main_pretrain.py
@@ -136,7 +136,7 @@ def main():
         total_params = sum(p.numel() for p in model.parameters())
         print(f"Number of parameters: {total_params}")
 
-
+        torch.cuda.empty_cache()
         with torch.profiler.profile(
                 schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
                 on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/memory_profile'),
@@ -190,7 +190,7 @@ def main():
                     # print(f"len of test labels: ", labels)
                     #torch.save(UQ_l, UQ_l_file)
                 
-                if epoch % 10 == 0:
+                if epoch % 20 == 0:
                     checkpoint_file = os.path.join(
                         "./checkpoints",
                         '{}_{}_{}_{}_epochs{}_{}heads_lamda1{}_lamda2{}.pth'.format(opt.model, model.head_type, opt.dataset, i, epoch, opt.nh,
@@ -199,7 +199,7 @@ def main():
                     torch.save(model.state_dict(), checkpoint_file)
                     
 
-                torch.cuda.empty_cache()
+                #torch.cuda.empty_cache()
 
         
         linegraph_minmax_area(std_data, opt.epochs)

UncertaintyAware-SSL/utils/losses.py
@@ -59,8 +59,7 @@ class UALoss(nn.Module):
         # tile mask
         mask = mask.repeat(anchor_count, contrast_count)
         # mask-out self-contrast cases
-        logits_mask = torch.scatter(
-            torch.ones_like(mask),
+        logits_mask = torch.ones_like(mask).scatter_(
             1,
             torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
             0